aefb79263090475ade2f98970cf06a32305e64be
Dao Nguyen <dorothyng284@gmail.com>
2024-10-02 16:22:35 -0700

    Add project files.

---
+++ CST350_Milestone/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CST350_Milestone.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
