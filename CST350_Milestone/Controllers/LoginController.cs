6284bc93647a18acd557de7638fca4ab568bf8dd
Dao Nguyen <dorothyng284@gmail.com>
2024-10-02 23:04:10 -0700

    Commit LoginFailure view and LoginSuccess view

--- CST350_Milestone/Controllers/LoginController.cs
+++ CST350_Milestone/Controllers/LoginController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CST350_Milestone.Models;
+using CST350_Milestone.Services.Business;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CST350_Milestone.Controllers
 {
     public class LoginController : Controller
     {
+        // Instantiate the UserCollection class and create an object of named users
+        static UserCollection users = new UserCollection();
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// Controller that will be called to process the user login
+        /// </summary>
+        /// <param name="loginViewModel"></param>
+        /// <returns></returns>
+        public IActionResult ProcessLogin(LoginViewModel loginViewModel)
+        {
+            // Declare and Initialize 
+            int result = -1;
+
+            // Is there a match
+            result = users.CheckCredentials(loginViewModel.Username, loginViewModel.Password);
+
+            // We know the result will be 0 if the cred check failed
+            if (result >= 0)
+            {
+                // Get the result and return it with the "UserModel"
+                UserModel user = users.GetUserById(result);
+                return View("LoginSuccess", user);
+            }
+            // There is no need for an else
+            return View("LoginFailure");
+
         }
     }
 }
