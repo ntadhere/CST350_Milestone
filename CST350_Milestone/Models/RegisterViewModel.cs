5c0d12fe9072b4f91a4deeaae2be4a94cd17c26c
Dao Nguyen <dorothyng284@gmail.com>
2024-10-09 14:16:45 -0700

    Commit create RegisterViewModel class

---
+++ CST350_Milestone/Models/RegisterViewModel.cs
@@ -0,0 +1,40 @@
+﻿namespace CST350_Milestone.Models
+{
+    public class SexViewModel
+    {
+        public bool IsSelected { get; set; }
+        public string Sex { get; set; }
+    }
+    public class RegisterViewModel
+    {
+
+        // Properties for Entry Screen
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string State { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public List<SexViewModel> Sex { get; set; }
+
+        public RegisterViewModel()
+        {
+            // Declare and Init
+            FirstName = "";
+            LastName = "";
+            Age = 0;
+            State = "";
+            Email = "";
+            UserName = "";
+            Password = "";
+            // Create the selection we want for checkboxes
+            Sex = new List<SexViewModel>
+            {
+                new SexViewModel{Sex = "Male", IsSelected = false},
+                new SexViewModel{Sex = "Felmale", IsSelected = false},
+                new SexViewModel{Sex = "Prefer not to say", IsSelected = false},
+            };
+        }
+    }
+}
