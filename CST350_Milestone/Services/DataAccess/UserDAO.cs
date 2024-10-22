319cc0f4716997077a853c9ac056454a89f25d00
Ashley Barron <ashleybarron292@gmail.com>
2024-10-21 20:17:19 -0700

    User DAO

---
+++ CST350_Milestone/Services/DataAccess/UserDAO.cs
@@ -0,0 +1,18 @@
+﻿namespace CST350_Milestone.Services.DataAccess
+{
+    public class UserDAO
+    {
+        // Define the connection for MSQL
+        // change userauth?
+        static string conn = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=UserAuth;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        // Define the connection string for MySQL
+        static string serverName = "localhost";
+        static string username = "root";
+        static string password = "root";
+        static string dbName = "userauth";
+        static string port = "3306";    // get correct port
+
+
+    }
+}
