4833b4e28a016f40127bd069b8cec8ad609b8d0b
Dao Nguyen <dorothyng284@gmail.com>
2024-11-05 09:45:02 -0700

    Revel button after click

--- CST350_Milestone/wwwroot/js/site.js
+++ CST350_Milestone/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function showLabel(button) {
+    // Prevent the form from submitting immediately to allow the label to be shown
+    event.preventDefault();
+
+    // Find the label inside the clicked button and remove the hidden class
+    const label = button.querySelector('.button-label');
+    label.classList.remove('hidden-label');
+
+    // Optionally submit the form after revealing the label
+    // button.closest("form").submit();
+}
