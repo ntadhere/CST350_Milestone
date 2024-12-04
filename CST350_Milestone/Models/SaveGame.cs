289667b37bee1d6c6266d9221494f361c2478150
Ashley Barron <ashleybarron292@gmail.com>
2024-12-04 08:17:07 -0700

    Creating Properties & Constructor

---
+++ CST350_Milestone/Models/SaveGame.cs
@@ -0,0 +1,22 @@
+﻿namespace CST350_Milestone.Models
+{
+    public class SaveGame
+    {
+
+        // *fix properties*!!
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string DataSaved { get; set; }
+        public string GameData { get; set; }
+
+
+        // *Fix Constructor!!*
+        public SaveGame(int id, int userId, string dataSaved, string gameData)
+        {
+            Id = id;
+            UserId = userId;
+            DataSaved = dataSaved;
+            GameData = gameData;
+        }
+    }
+}
